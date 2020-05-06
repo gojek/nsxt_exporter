26ef275a3fa40e0232eaa0a4278f81b76879ea84
Giri Kuncoro <girikuncoro@gmail.com>
2020-05-06 11:28:55 +0700

    Add dummy collector and exporter describe
    This adds dummy metrics to see if
    the collector and exporter registertration
    is sucessful.
    
    Signed-off-by: Giri Kuncoro <girikuncoro@gmail.com>
    Signed-off-by: William Albertus Dembo <w.albertusd@gmail.com>

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/prometheus/client_golang v1.0.0
 	github.com/prometheus/common v0.9.1
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	k8s.io/apimachinery v0.18.2 // indirect
 )
--- nsxt_exporter.go
+++ nsxt_exporter.go
@@ -5,11 +5,46 @@ import (
 	"os"
 
 	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
+
+const (
+	namespace = "nsxt"
+)
+
+var (
+	up = prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "up"), "Is the NSX-T server healthy.", nil, nil)
+)
+
+type Exporter struct {
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- up
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	ok := e.collectHealthStateMetric(ch)
+	if ok {
+		ch <- prometheus.MustNewConstMetric(up, prometheus.GaugeValue, 1.0)
+	} else {
+		ch <- prometheus.MustNewConstMetric(up, prometheus.GaugeValue, 0.0)
+	}
+}
+
+func (e *Exporter) collectHealthStateMetric(ch chan<- prometheus.Metric) bool {
+	// TODO(giri/william): Collect metrics and push to channel before returning true
+	return true
+}
+
+func NewExporter() (*Exporter, error) {
+	return &Exporter{}, nil
+}
 
 func main() {
 	var (
@@ -21,6 +56,17 @@ func main() {
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
 	logger := promlog.New(promlogConfig)
+
+	level.Info(logger).Log("msg", "Starting nsxt_expoter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "context", version.BuildContext())
+
+	exporter, err := NewExporter()
+	if err != nil {
+		level.Error(logger).Log("msg", "Error creating the exporter", "err", err)
+		os.Exit(1)
+	}
+	prometheus.MustRegister(exporter)
+	prometheus.MustRegister(version.NewCollector("nsxt_exporter"))
 
 	level.Info(logger).Log("msg", "Listening on address", "address", *listenAddress)
 	http.Handle(*metricsPath, promhttp.Handler())
