7e4296788cdae08ba12f3c864238dc9b7af76062
Giri Kuncoro <girikuncoro@gmail.com>
2020-05-12 10:44:48 +0700

    Replace the use of nsxtClient with interface for DHCP collector
    The nsxtClient has an interface now for easy pluggability. This commit
    introduces the use of interface for DHCP collector.
    
    Signed-off-by: Giri Kuncoro <girikuncoro@gmail.com>

--- client/types.go
+++ client/types.go
@@ -2,8 +2,15 @@ package client
 
 import "github.com/vmware/go-vmware-nsxt/manager"
 
+// LogicalPortClient represents API group logical port for NSX-T client.
 type LogicalPortClient interface {
 	ListLogicalPorts(localVarOptionals map[string]interface{}) (manager.LogicalPortListResult, error)
 	GetLogicalPortStatusSummary(localVarOptionals map[string]interface{}) (manager.LogicalPortStatusSummary, error)
 	GetLogicalPortOperationalStatus(lportId string, localVarOptionals map[string]interface{}) (manager.LogicalPortOperationalStatus, error)
+}
+
+// DHCPClient represents API group DHCP for NSX-T client.
+type DHCPClient interface {
+	ListDhcpServers(localVarOptionals map[string]interface{}) (manager.LogicalDhcpServerListResult, error)
+	GetDhcpStatus(dhcpID string, localVarOptionals map[string]interface{}) (manager.DhcpServerStatus, error)
 }
