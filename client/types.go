4a22e0113beb23e120f07062cfa9140e2f6bd3e5
William Albertus Dembo <william.dembo@go-jek.com>
2020-05-12 10:28:01 +0700

    Move client interfact to types.go for better readibility

--- client/types.go
+++ client/types.go
@@ -1,8 +1,6 @@
 package client
 
-import (
-	"github.com/vmware/go-vmware-nsxt/manager"
-)
+import "github.com/vmware/go-vmware-nsxt/manager"
 
 type LogicalPortClient interface {
 	ListLogicalPorts(localVarOptionals map[string]interface{}) (manager.LogicalPortListResult, error)
